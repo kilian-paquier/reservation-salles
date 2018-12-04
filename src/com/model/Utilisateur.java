0587b051a220d6f4cec98a15b92f199a3c1c5280
Kilian <kilian.paquier@hotmail.fr>
2018-12-04 10:44:05 +0100

    classes

---
+++ src/com/model/Utilisateur.java
@@ -0,0 +1,13 @@
+package com.model;
+
+import java.util.List;
+
+public class Utilisateur {
+    private int id;
+    private String prenom;
+    private String nom;
+    private String mail;
+    private List<Salle> reservations;
+
+
+}
--- src/com/view/Main.java
+++ src/com/view/Main.java
@@ -1,4 +1,7 @@
 package com.view;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
