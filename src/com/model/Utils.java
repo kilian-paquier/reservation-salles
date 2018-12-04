8e3da2e2f61ebc79e4f66a4a9b31c778ff10d937
Kilian <kilian.paquier@hotmail.fr>
2018-12-04 14:36:17 +0100

    controler

--- src/com/model/Utils.java
+++ src/com/model/Utils.java
@@ -4,7 +4,8 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.sql.*;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Utils {
@@ -206,8 +207,8 @@ public abstract class Utils {
             md.update(password.getBytes());
             byte[] bytes = md.digest();
             StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < bytes.length; i++) {
-                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            for (byte aByte : bytes) {
+                sb.append(Integer.toString((aByte & 0xff) + 0x100, 16).substring(1));
             }
             return sb.toString();
         } catch (NoSuchAlgorithmException e) {
